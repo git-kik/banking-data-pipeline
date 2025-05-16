d4690617806a8ef358ab1d23e4e0d741b413d6a9
git-kik <dslearn678@gmail.com>
2025-05-16 16:23:21 +0545

    week1T2: load raw data pg

--- data/migrate_data_to_local_raw.py
+++ data/migrate_data_to_local_raw.py
@@ -1,42 +1,40 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+import pandas as pd
+from urllib.parse import quote_plus
 
 load_dotenv()
 
-Dlytica_postgres = {
-    'host': os.getenv('dlytica_host'),
-    'port' : os.getenv('dlytica_port'),
-    'dbname' : os.getenv('dlytica_dbname'),
-    'user': os.getenv('dlytica_user'),
-    'password' : os.getenv('dlytica_pass')
-}
+password = quote_plus(os.getenv('local_pass'))
 
-local_postgres = {
-    'host': os.getenv('local_host'),
-    'port' : os.getenv('local_port'),
-    'dbname' : os.getenv('local_dbname'),
-    'user': os.getenv('local_user'),
-    'password' : os.getenv('local_pass')
-}
+dlytica_postgres_url = f"postgresql://{os.getenv('dlytica_user')}:{os.getenv('dlytica_pass')}@{os.getenv('dlytica_host')}:{os.getenv('dlytica_port')}/{os.getenv('dlytica_dbname')}"
+local_postgres_url = f"postgresql://{os.getenv('local_user')}:{password}@{os.getenv('local_host')}:{os.getenv('local_port')}/{os.getenv('local_dbname')}"
+
+tables = ['transaction', 'account', 'customer', 'cards', 'product']
 
 
-def create_schema():
-    conn = psycopg2.connect(**local_postgres)
-    print("Connected to DB:", conn.get_dsn_parameters()['dbname']) 
-    cursor = conn.cursor()
+def fetch_and_load_sql_table():
+    local_engine = create_engine(local_postgres_url)
 
-    cursor.execute("CREATE SCHEMA IF NOT EXISTS raw;")
-    cursor.execute("CREATE SCHEMA IF NOT EXISTS staging;")
-    print("Schemas created succesfully")
+    with local_engine.begin() as conn:
+        conn.execute(text("CREATE SCHEMA IF NOT EXISTS raw;"))
+        conn.execute(text("CREATE SCHEMA IF NOT EXISTS staging;"))
+        print("Schemas created succesfully")
 
-    conn.commit()
-    cursor.close()
-    conn.close()
+    dlytica_engine = create_engine(dlytica_postgres_url)
 
-def main():
-    create_schema()
-    
+    for table in tables:
+        df = pd.read_sql_table(table,dlytica_engine)
+
+        df.to_sql(table,
+                con=local_engine,
+                schema='raw',
+                if_exists='replace',
+                index=False)
+        print(f"Data of table:{table} migrated")
+
 
 if __name__ == '__main__':
-    main()
+    fetch_and_load_sql_table()
